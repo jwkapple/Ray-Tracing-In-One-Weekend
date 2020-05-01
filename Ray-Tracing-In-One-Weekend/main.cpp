3cbdd95866cdb6c9b5a27ca2a909b709f9987414
Jiwon Kang <jwkorange@gmail.com>
2020-05-01 17:01:35 +0900

    [Update] Chapter 5.Adding a Sphere

--- Ray-Tracing-In-One-Weekend/main.cpp
+++ Ray-Tracing-In-One-Weekend/main.cpp
@@ -2,8 +2,25 @@
 #include "vec3.h"
 #include "ray.h"
 
+bool hitSphere(const vec3& center, double radius, const ray& ray)
+{
+	vec3 oc = ray.origin() - center;
+	auto a = dot(ray.direction(), ray.direction());
+	auto b = 2.0 * dot(oc, ray.direction());
+	auto c = dot(oc, oc) - radius * radius;
+
+	auto discriminant = b * b - 4 * a * c;
+
+	return (discriminant >= 0);
+}
+
 vec3 rayColor(const ray &r)
 {
+	if (hitSphere(vec3(0, 0, -1), 0.5, r))
+	{
+		return vec3(1.0, 0.0, 0.0);
+	}
+
 	vec3 unitDirection = unit_vector(r.direction());
 	auto t = 0.5 * (unitDirection.y() + 1.0);
 	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
@@ -30,7 +47,7 @@ int main()
 			auto v = double(j) / imageHeight;
 
 			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-			
+
 			vec3 color = rayColor(r);
 
 			color.write_color(std::cout);
