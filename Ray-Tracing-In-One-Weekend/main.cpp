50536dbecba923972e1ba360f02fbf683f25fe94
Jiwon Kang <jwkorange@gmail.com>
2020-05-01 15:58:15 +0900

    [Update] Chapter 4.Rays, a Simple Camera, and Background

--- Ray-Tracing-In-One-Weekend/main.cpp
+++ Ray-Tracing-In-One-Weekend/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 #include "vec3.h"
+#include "ray.h"
+
+vec3 rayColor(const ray &r)
+{
+	vec3 unitDirection = unit_vector(r.direction());
+	auto t = 0.5 * (unitDirection.y() + 1.0);
+	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
 
 int main()
 {
@@ -8,12 +16,23 @@ int main()
 
 	std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
 	
+	vec3 lower_left_corner(-2.0, -1.0, -1.0);
+	vec3 horizontal(4.0, 0.0, 0.0);
+	vec3 vertical(0.0, 2.0, 0.0);
+	vec3 origin(0.0, 0.0, 0.0);
+
 	for (int j = imageHeight-1; j >= 0; --j)
 	{
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 		for (int i = 0; i < imageWidth; ++i)
 		{
-			vec3 color{ double(i) / imageWidth, double(j) / imageHeight, 0.2 };
+			auto u = double(i) / imageWidth;
+			auto v = double(j) / imageHeight;
+
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+			
+			vec3 color = rayColor(r);
+
 			color.write_color(std::cout);
 		}
 	}
